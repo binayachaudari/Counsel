771d1c0d015b974ebf038901590bcfaa12f54398
Binaya Chaudhary <binayachaudari@live.com>
2019-07-07 08:59:55 +0545

    Responsive Design

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ const navBar = () => {
   const nav = document.querySelector('.nav-links');
 
   const navLinks = document.querySelectorAll('.nav-links li');
+  const burgerLines = burger.querySelectorAll('div');
 
   burger.addEventListener('click', (e) => {
+
+    burgerLines.forEach((line, index) => {
+      line.style.transition = 'all 0.5s ease';
+    })
+
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
+    nav.style.transition = 'all 0.4s ease-in';
   })
 
   navLinks.forEach((navItem, index) => {
